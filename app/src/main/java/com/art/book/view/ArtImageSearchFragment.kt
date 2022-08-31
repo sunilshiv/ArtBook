d4a9dea03827f8efa23110ed047c7b54b952c5f4
Sunil D S <sunil.d.shiv@gmail.com>
2022-08-31 15:17:02 +0530

    Added features for navigation between fragments

---
+++ app/src/main/java/com/art/book/view/ArtImageSearchFragment.kt
@@ -0,0 +1,13 @@
+package com.art.book.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.art.book.R
+
+class ArtImageSearchFragment: Fragment(R.layout.fragment_image_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
