a6ca45a9bedc97f137287cb69594328bd3b9ef7d
Sunil D S <sunil.d.shiv@gmail.com>
2022-08-31 16:16:24 +0530

    Add files related to room database

---
+++ app/src/main/java/com/art/book/dao/ArtDao.kt
@@ -0,0 +1,18 @@
+package com.art.book.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.art.book.model.Art
+
+@Dao
+interface ArtDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArt(art: Art)
+
+    @Delete
+    suspend fun deleteArt(art: Art)
+
+    @Query("SELECT * FROM arts")
+    fun observeArts(): LiveData<List<Art>>
+}
