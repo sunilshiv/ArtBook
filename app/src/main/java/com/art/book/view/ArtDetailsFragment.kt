d3e3375cbb4290aae00844aec6e3513bb24459c4
Sunil D S <sunil.d.shiv@gmail.com>
2022-08-31 15:51:45 +0530

    Updated ArtDetailsFragment with navigation

--- app/src/main/java/com/art/book/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/art/book/view/ArtDetailsFragment.kt
@@ -2,12 +2,39 @@ package com.art.book.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.art.book.R
+import com.art.book.databinding.FragmentArtDetailsBinding
 
 class ArtDetailsFragment: Fragment(R.layout.fragment_art_details) {
 
+    private var fragmentArtDetailsBinding: FragmentArtDetailsBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtDetailsBinding.bind(view)
+        fragmentArtDetailsBinding = binding
+
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToArtImageSearchFragment())
+        }
+
+        val callBack = object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
+
+    }
+
+    override fun onDestroy() {
+        fragmentArtDetailsBinding = null
+        super.onDestroy()
     }
 }
