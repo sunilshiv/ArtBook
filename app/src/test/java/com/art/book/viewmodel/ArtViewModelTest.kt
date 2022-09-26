f92fd20087f8f14e51ed6d193ca63eca9d725c36
Sunil D S <sunil.d.shiv@gmail.com>
2022-09-26 20:59:09 +0530

    Add LiveData unit testing functionality

--- app/src/test/java/com/art/book/viewmodel/ArtViewModelTest.kt
+++ app/src/test/java/com/art/book/viewmodel/ArtViewModelTest.kt
@@ -1,16 +1,50 @@
 package com.art.book.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.art.book.MainCoroutineRule
+import com.art.book.getOrAwaitValueTest
 import com.art.book.repository.FakeArtRepository
+import com.art.book.utils.Status
+import com.google.common.truth.Truth.assertThat
+
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class ArtViewModelTest {
+
+    @get: Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     private lateinit var viewModel: ArtViewModel
 
     @Before
     fun setUp() {
         //Test Doubles
-
         viewModel = ArtViewModel(FakeArtRepository())
+    }
+
+    @Test
+    fun `insert art without year returns error`() {
+        viewModel.makeArt("Mona Lisa", "Da Vinci", "")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art without name returns error`() {
+        viewModel.makeArt("", "Da Vinci", "1800")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art without artistName returns error`() {
+        viewModel.makeArt("Mona Lisa", "", "1800")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
     }
 }
